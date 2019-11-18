11ce14bae579922388a2a9c927d84b7cce30fa2e
maratkurbangaleev <mkurbangaleev@gmail.com>
2019-11-18 09:08:02 +0300

    Delete seminar2_join.sql

--- seminar2_join.sql
+++
@@ -1,9 +0,0 @@
--- Запрос join со второго семинара по SQL и БД
-
-select DISTINCT "EMITENT_FULL_NAME"
-from public.emitent INNER JOIN 
-	(select DISTINCT emt_id 
-	from public.sec_listed inner JOIN public.order_log
-	ON public.sec_listed."TRADE_CODE"=public.order_log."SECCODE") as some_name
-	ON public.emitent.emt_id=some_name.emt_id
-	;
